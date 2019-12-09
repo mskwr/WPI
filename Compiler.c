7b9b0b9699934d9b709076db6dc5016e22e2f92f
Michał Skwarek <57644287+presnelberg@users.noreply.github.com>
2019-12-09 11:44:23 +0100

    Update Compiler.c

--- Compiler.c
+++ Compiler.c
@@ -20,8 +20,90 @@ int load() {
     return c;
 }
 
+int analize(int **tab, int *i, int *size, int *cell) {
+    int c=0;
+    int i=0;
+    int var=0;
+    if(c=='{') {
+        while((c=load())!='}') {
+           if(i==*cell) {
+            cell=1+*cell*2;;
+            tab=realloc(tab, *cell * sizeof *tab);
+           }
+           if(c>='a' && c<='z') {
+               var=0;
+               tab[tab[i]]=c+3;
+           }
+           else if(c=='$') {
+               var=1;
+               tab[tab[i]]=50;
+           }
+           else if(c=='+') {
+               if(var==0) tab[tab[i]]=1;
+               else tab[tab[i]]=3;
+           }
+           else if(c=='-') {
+               if(var==0) tab[tab[[i]]]=0;
+               else tab[tab[i]]=2;
+           }
+           else if(c=='{') {
+               if(var==0) {
+                   tab[tab[i]]=4;
+                   i++;
+                   *tab=analize(tab, &i, &size);
+                   tab[tab[i]]=6;
+                   *tab=analize(tab, &i, &size);
+               }
+               else {
+                   tab[tab[i]]=5;
+                   i++;
+                   *tab=analize(tab, i, size, cell);
+                   tab[tab[i]]=6;
+                   *tab=analize(tab, i, size, cell);
+               }
+           }
+           else if(c>='A' && c<='Z') {
+               tab[tab[i]]=7;
+               i++;
+               tab[tab[i]]=-200-c;
+           }
+           i++;
+           tab[tab[i]]=8;
+           i++;
+           **size=i;
+        }
+    }
+    return *tab;
+}
+
+int compile(int *size) {
+    int *tab=NULL;
+    int i=0;
+    int c=0;
+    int cell=0;
+    for(i=0; (c=load())!=EOF; i++) {
+        if(i==cell) {
+            cell=1+cell*2;;
+            tab=realloc(tab, cell * sizeof *tab);
+        }
+        if(c>='A' && c<='Z') {
+            tab[i]=-100-c;
+            i++;
+            *tab=analize(tab, &i, size, &cell);
+        }
+        else {
+            tab[i]=-1000;
+            i++;
+            *tab=analize(tab, &i, size, &cell);
+        }
+    }
+    return tab;
+}
+
 int main(void)
 {
-    int cell=0;
+    int size=0;
+    int *compiler=compile(&size);
+    for(int i=0; i<size; i++) printf("%d", compiler[i]);
     return 0;
 }
